41a7e9b40dfdd1b426bba8ef9bb67354990f75dd
Pepe <jose.duran@mail.udp.cl>
2021-07-09 18:48:15 -0400

    test db

--- demo_mysql_test.py
+++ demo_mysql_test.py
@@ -1,1 +1,9 @@
 import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="btayogimwuf6ppd8dtgu-mysql.services.clever-cloud.com",
+    user="uzkqujyps8yvnfwd",
+    password="LMRyKb5ygYFGs32bg0W5"
+)
+
+print(mydb)
