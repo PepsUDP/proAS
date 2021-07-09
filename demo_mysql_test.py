5487cd804977d399dfddf17d2fa9dc19736891a2
Pepe <jose.duran@mail.udp.cl>
2021-07-09 19:10:21 -0400

    test db

--- demo_mysql_test.py
+++ demo_mysql_test.py
@@ -3,14 +3,15 @@ import mysql.connector
 mydb = mysql.connector.connect(
     host="btayogimwuf6ppd8dtgu-mysql.services.clever-cloud.com",
     user="uzkqujyps8yvnfwd",
-    password="LMRyKb5ygYFGs32bg0W5"
+    password="LMRyKb5ygYFGs32bg0W5",
+    database="btayogimwuf6ppd8dtgu"
 )
 
 print(mydb)
 
 mycursor = mydb.cursor()
 
-mycursor.execute("SHOW DATABASES")
+mycursor.execute("SHOW TABLES")
 
 for x in mycursor:
   print(x)
