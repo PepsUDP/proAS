16d37c85dfe7959d3a51da27d24d832050924039
Pepe <jose.duran@mail.udp.cl>
2021-07-10 20:20:13 -0400

    rgtrUser test

--- services/srv_registro.py
+++ services/srv_registro.py
@@ -14,7 +14,7 @@ def registerU(rgtr):
     if fetched == None:
         if rgtr["rol"] in ["1","2"]:
             rol = "administrador" if rgtr["rol"] == "1" else "general"
-            dbuci.execute("INSERT INTO users (username, password, rol) VALUES(?, ?, ?)", (rgtr["username"],rgtr["password"], rol))
+            dbuci.execute("INSERT INTO users (username, password, rol) VALUES(%s, %s, %s)", (rgtr["username"],rgtr["password"], rol))
             dbuci.commit()
             response = {"respuesta":"El usuario ha sido registrado exitosamente."}
             sendT(sckt, json.dumps(response), srv)
