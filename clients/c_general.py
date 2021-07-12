f3e074fcef24d8159aa5cceffd2fb80f25fe1e3b
Pepe <jose.duran@mail.udp.cl>
2021-07-11 20:30:05 -0400

    srv_login test

---
+++ clients/c_general.py
@@ -0,0 +1,154 @@
+import socket, sys, json
+from os import system, name
+from comunicacion import clearS, sendT, listenB
+rgtr = "sgnup"  #Registro
+lgin = "ccdli" #Ingreso
+
+sesion = {"username":None,"password":None,"rol":None}
+sock = None
+
+def menuSULI():
+    clearS()
+    menu = """
+    ***************************************
+    * Usuario general                     *
+    *-------------------------------------*
+    * Elija una opción:                   *
+    * 1) Sign up (Registrar un usuario)   *
+    * 2) Log in (Ingresar con usuario)    *
+    ***************************************
+
+    Opción: """
+    option = input(menu)
+    if option == "1":
+        menuSU()
+    elif option =="2":
+        menuLI()
+    else:
+        print("Opción ingresada no válida.")
+        menuSULI()
+
+def menuSU():
+    username = None
+    password = None
+    rol = 2 # Usuario general
+    clearS()
+
+    menuUN = """
+    ***************************************
+    * Usuario general                     *
+    *-------------------------------------*
+    * Registro de usuario                 *
+    * Ingresar nombre de usuario          *
+    ***************************************
+
+    Usuario: """    
+    clearS()
+    username = input(menuUN)
+
+    menuPW = """
+    ***************************************
+    * Usuario general                     *
+    *-------------------------------------*
+    * Registro de usuario                 *
+    * Ingresar contraseña                 *
+    ***************************************
+    
+    Contraseña: """
+    clearS()
+    password = input(menuPW)
+
+    menuYN = f"""
+    ***************************************
+    * Usuario general                     *
+    *-------------------------------------*
+    * Registro de usuario                 *
+    * Confirme sus datos [y/n]            *
+    ***************************************
+    
+    Usuario: {username}
+    Contraseña: {password}
+    Rol: {"Administrador" if rol == "1" else "General"}
+    
+    Opción: """
+    clearS()
+    yn = input(menuYN)
+    if yn == "y":
+        arg = {"username": username, "password": password, "rol": rol}
+        print(arg)
+        sendT(sckt, rgtr, json.dumps(arg))
+        nS, msgT = listenB(sckt)
+        print(msgT)
+        msg =  json.loads(msgT[12:])
+        print(nS)
+        print(msgT)
+        if nS == rgtr:
+            if msg["respuesta"]:
+                print(msg["respuesta"])
+    else:
+        menuSU()
+
+def menuLI():
+    username = None
+    password = None
+    #rol = 2 # Usuario general
+
+    menuUN = """
+    ***************************************
+    * Usuario general                     *
+    *-------------------------------------*
+    * Inicio de sesión                    *
+    * Ingresar nombre de usuario          *
+    ***************************************
+
+    Usuario: """   
+    clearS()
+    username = input(menuUN)
+
+    menuPW = """
+    ***************************************
+    * Usuario general                     *
+    *-------------------------------------*
+    * Inicio de sesión                    *
+    * Ingresar contraseña                 *
+    ***************************************
+    
+    Contraseña: """
+    clearS()
+    password = input(menuPW)
+
+    arg = {"username": username, "password": password}
+    #arg = {"username": username, "password": password, "rol": rol}
+    print(arg)
+    sendT(sock, lgin, json.dumps(arg))
+    nS, msgT=listenB(sckt)
+    print(msgT)
+    msg = json.loads(msgT[12:])
+    print(msg)
+    if nS == lgin:
+        if msg["respuesta"] == "No es posible entrar con el usuario ingresado.":
+            input("No se ha podido iniciar sesión.")
+            menuLI() 
+        else:
+            global sesion
+            sesion=msg["respuesta"]
+            print(sesion)
+            if sesion["rol"] == "2":
+                # Menu cliente
+                print("menuCliente()")
+            else:
+                print("entre a un else")
+                #menuIngresar()
+
+if __name__ == "__main__":
+    try:
+        sckt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        server_address = ('localhost', 5000)
+        print('Cliente: Conectandose a {} puerto {}'.format(*server_address))
+        sckt.connect(server_address)
+    except: 
+        print('No es posible la conexión al bus')
+        quit()
+        
+    #menuIngresar()
