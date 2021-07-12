831194b494b8f1c05dacc6d4df1fcdc82ad92203
Pepe <jose.duran@mail.udp.cl>
2021-07-12 19:33:24 -0400

    elie test

--- services/srv_ccdee.py
+++ services/srv_ccdee.py
@@ -9,37 +9,37 @@ def eliE(opcion, rgtr):
     fetched = None
     print(rgtr)
     if opcion == 1:
-        crsr.execute("DELETE FROM pasillo WHERE id_pasillo = %s", (rgtr))
+        crsr.execute("DELETE FROM pasillo WHERE id_pasillo = %s", (rgtr,))
         dbuci.commit()
         response = {"respuesta":"El pasillo ha sido eliminado exitosamente."}
         sendT(sckt, srv, json.dumps(response))
     elif opcion == 2:
         print(rgtr)
-        crsr.execute("DELETE FROM sala WHERE id_sala=%s", (rgtr))
+        crsr.execute("DELETE FROM sala WHERE id_sala=%s", (rgtr,))
         dbuci.commit()
         response = {"respuesta":"La pieza ha sido eliminado exitosamente."}
         sendT(sckt, srv, json.dumps(response))
     elif opcion == 3:
         print(rgtr)
-        crsr.execute("DELETE FROM personalLimpieza WHERE RUT=%s", (rgtr))
+        crsr.execute("DELETE FROM personalLimpieza WHERE RUT=%s", (rgtr,))
         dbuci.commit()
         response = {"respuesta":"El empleado de limpieza ha sido eliminado exitosamente."}
         sendT(sckt, srv, json.dumps(response))
     elif opcion == 4:
         print(rgtr)
-        crsr.execute("DELETE FROM paciente WHERE RUT=%s", (rgtr))
+        crsr.execute("DELETE FROM paciente WHERE RUT=%s", (rgtr,))
         dbuci.commit()
         response = {"respuesta":"El paciente ha sido eliminado exitosamente."}
         sendT(sckt, srv, json.dumps(response))
     elif opcion == 5:
         print(rgtr)
-        crsr.execute("DELETE FROM personalMedico WHERE RUT=%s", (rgtr))
+        crsr.execute("DELETE FROM personalMedico WHERE RUT=%s", (rgtr,))
         dbuci.commit()
         response = {"respuesta":"El medico ha sido eliminado exitosamente."}
         sendT(sckt, srv, json.dumps(response))
     elif opcion == 6:
         print(rgtr)
-        crsr.execute("DELETE FROM equipoMedico WHERE id_equipoMedico=%s", (rgtr))
+        crsr.execute("DELETE FROM equipoMedico WHERE id_equipoMedico=%s", (rgtr,))
         dbuci.commit()
         response = {"respuesta":"La herramienta medica ha sido eliminada exitosamente."}
         sendT(sckt, srv, json.dumps(response))
