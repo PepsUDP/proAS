d6f3af4e271778032da95dae9f18b31971100047
Pepe <jose.duran@mail.udp.cl>
2021-07-12 14:14:19 -0400

    gdata test

--- services/srv_getdb.py
+++ services/srv_getdb.py
@@ -3,40 +3,60 @@ from db_uci import dbuci
 from comunicacion import sendT, listenB, registerS
 srv = 'ccddb'
 
+class create_dict(dict):
+    def __init__(self):
+        self = dict()
+
+    def add(self, key, value):
+        self[key] = value
+
 # Obtener datos
 def getData(opcion):
+    mydict = create_dict()
     crsr = dbuci.cursor()
     print(opcion)
     fetched = None
     if opcion == 1:
         crsr.execute("SELECT * FROM pasillo;")   
         fetched = crsr.fetchall()
+        for row in fetched:
+            mydict.add(row[0],({"estado":row[1]}))
     elif opcion == 2:
         crsr.execute("SELECT * FROM sala;")
-        fetched = crsr.fetchall()     
+        fetched = crsr.fetchall()
+        for row in fetched:
+            mydict.add(row[0],({"cantCamas":row[1],"camasDisp":row[2],"estado":row[3]}))
     elif opcion == 3:
         crsr.execute("SELECT * FROM equipoMedico WHERE tipo='cama';")   
-        fetched = crsr.fetchall() 
+        fetched = crsr.fetchall()
+        for row in fetched:
+            mydict.add(row[0],({"u_paciente_RUT":row[1],"tipo":row[2],"fechaInicio":row[3],"tiempoUso":row[4],"estado":row[5]}))
     elif opcion == 4:
         crsr.execute("SELECT * FROM paciente;") 
         fetched = crsr.fetchall()
+        for row in fetched:
+            mydict.add(row[0],({"nombre":row[1],"fechanac":row[2],"edad":row[3],"enfermedad":row[4],"sintomas":row[5],"dieta":row[6],"alergias":row[7],"medicamentos":row[8],"tratamiento":row[9]}))
     elif opcion == 5:
         crsr.execute("SELECT * FROM personalMedico;")
         fetched = crsr.fetchall()
+        for row in fetched:
+            mydict.add(row[0],({"nombre":row[1],"fechanac":row[2],"especialidad":row[3],"disponible":row[4]}))
     elif opcion == 6:    
         crsr.execute("SELECT * FROM equipoMedico WHERE tipo='respirador';")
         fetched = crsr.fetchall()
+        for row in fetched:
+            mydict.add(row[0],({"u_paciente_RUT":row[1],"tipo":row[2],"fechaInicio":row[3],"tiempoUso":row[4],"estado":row[5]}))
     if fetched:
         print(fetched)
         print(type(fetched))
 
-        response = json.dumps(fetched)
+        response = json.dumps(mydict, indent=2, sort_keys=True)
         print(response)
         print(type(response))
-        test = '{' + response[1:-1] + '}'
-        print(test)
+        #test = '{' + response[1:-1] + '}'
+        #print(test)
         #response = {"respuesta":fetched}
-        sendT(sckt, srv, str(test))
+        sendT(sckt, srv, response)
     else:
         print("No sacó nada")
 
